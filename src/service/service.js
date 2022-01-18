72da1eccf07121c8af0a4d871ecfaa11cc091a4f
traine-octarine <65852581+train-octarine@users.noreply.github.com>
2022-01-19 02:41:14 +0300

    add third h/w

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {cli} = require(`./cli`);
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode,
+} = require(`../utils/constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+const commandArgument = userArguments.slice(1);
+
+if (userArguments.length === 0 || !cli[userCommand]) {
+  cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+cli[userCommand].run(commandArgument);
