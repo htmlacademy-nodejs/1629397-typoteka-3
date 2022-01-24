be2821e4d22df56b413cfb83e8de061497dfbfe0
traine-octarine <65852581+train-octarine@users.noreply.github.com>
2022-01-25 02:09:42 +0300

    add mock data to external files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -11,60 +11,23 @@ const {ExitCode} = require(`../../utils/constants`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const LINES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const restricts = {
   anounce: 5,
   mocks: 1000,
+};
+
+const readData = async (filepath) => {
+  try {
+    const data = await fs.readFile(filepath, `utf8`);
+    return data.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
 };
 
 const generateDate = () => {
@@ -75,13 +38,13 @@ const generateDate = () => {
   return randomDate.format(`YYYY-MM-DD hh:mm:ss`);
 };
 
-const generateArticles = (count) => (
+const generateArticles = (count, sentences, titles, categories) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    announce: shuffle(LINES).slice(0, getRandomInt(1, restricts.anounce)).join(` `),
-    fullText: shuffle(LINES).slice(0, getRandomInt(1, LINES.length - 1)).join(` `),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    announce: shuffle(sentences).slice(0, getRandomInt(1, restricts.anounce)).join(` `),
+    fullText: shuffle(sentences).slice(0, getRandomInt(1, sentences.length - 1)).join(` `),
     createdDate: generateDate(),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
+    category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
   }))
 );
 
@@ -96,7 +59,10 @@ module.exports = {
       process.exit(ExitCode.error);
     }
 
-    const content = JSON.stringify(generateArticles(countOffer));
+    const sentences = await readData(FILE_SENTENCES_PATH);
+    const titles = await readData(FILE_TITLES_PATH);
+    const categories = await readData(FILE_CATEGORIES_PATH);
+    const content = JSON.stringify(generateArticles(countOffer, sentences, titles, categories));
 
     try {
       await fs.writeFile(FILE_NAME, content);
